fdaf4f222714550460674fbbe656d84165aa3d50
ibrahimgulsoy <ibrahimglsy1@gmail.com>
2023-12-23 17:53:12 +0300

    project exercise

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,48 @@
 <template>
- <div class="app">
-  <p>{{ name  }}</p>
- </div>
+  <div class="app">      
+    <JobList :jobs="jobs"/>
+
+    <!-- <p>{{ jobs[0].title }} </p> -->
+    <!--<p>{{ name }} - {{ age }} {{ message }}</p>
+
+    <button @click="changeName('taha', 25)">change taha</button>
+    <button @click="changeName('ibrahim', 30)">change ibrahim</button>
+    <button @click="changeAge('20')">change age</button>
+    <input v-model="message" />-->
+  </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
-const name= ref('Link');
+import { ref } from "vue";
+import JobList from "./components/JobList.vue";
+import type Job from '@/types/Job'
+
+
+const name = ref("Link");
+const age = ref<number | string>(0);
+const message = ref('');
+components: {JobList}
+
+
+const jobs = ref<Job[]>([
+  { title: 'Farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+  { title: 'Quarryman', location: 'death mountain', salary: 40000, id: '2' },
+  { title: 'Flute player', location: 'the lost woods', salary: 35000, id: '3' },
+  { title: 'Fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+  { title: 'Prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+]);
+
+const changeName = (newName: string, newAge: number) => {
+  name.value = newName;
+  age.value = newAge;
+  changeAge(age.value);
+};
+
+const changeAge = (newAge: string | number) => {
+  age.value = newAge;
+};
 </script>
 
-<style >
-
+<style>
+ 
 </style>
---
+++ src/components/JobList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="job-list">
+    <ul>
+      <li v-for="job in props.jobs" :key="job.id">
+        {{ job.title }} - {{ job.location }} - Salary: {{ job.salary }}
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class=" salary " >
+          <p>{{ job.salary }} ruppes</p>
+        </div>
+        <div class="description"></div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nam
+          doloribus pariatur recusandae saepe! Dolore magni odio reprehenderit
+          velit tenetur eveniet neque odit mollitia iste sed et, qui doloremque
+          quia.
+        </p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+const props = defineProps(["jobs"]);
+</script>
+<style scoped>
+.job-list {
+  max-width: 960px;
+  margin: 40px auto;
+}
+.job-list ul {
+  padding: 0;
+}
+.job-list li {
+  list-style-type: none;
+  background: white;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+}
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+.salary {
+  display: flex;
+}
+.salary img {
+  width: 30px;
+}
+.salary p {
+  color: #17bf66;
+  font-weight: bold;
+  margin: 10px 4px;
+}
+</style>
