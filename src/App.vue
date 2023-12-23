6035bc336162821a67ebd6cc2c1a946a7b1708c3
ibrahimgulsoy <ibrahimglsy1@gmail.com>
2023-12-23 12:00:22 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+ <div class="app">
+  <p>{{ name  }}</p>
+ </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+const name= ref('Link');
+</script>
+
+<style >
+
+</style>
